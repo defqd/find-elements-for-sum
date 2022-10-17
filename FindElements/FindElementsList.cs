1a3c377a0672808731a1733db5a3f6b450d1ea8a
defqd <DefQD@yandex.ru>
2022-10-17 21:55:18 +0300

    Add find elements of list

--- FindElements/FindElementsList.cs
+++ FindElements/FindElementsList.cs
@@ -6,6 +6,44 @@
         {
             start = 0;
             end = 0;
+
+            int resultStart = 0;
+            int resultEnd = 0;
+
+            var currentSum = list[0];
+
+            while (start != list.Count - 1)
+            {
+                if (currentSum == sum)
+                {
+                    resultStart = start;
+                    resultEnd = end;
+
+                    if (start == list.Count - 2 || end == list.Count - 1)
+                        break;
+
+                    currentSum -= list[start];
+                    start++;
+                    end++;
+                    currentSum += list[end];
+                }
+
+                if (currentSum > sum)
+                {
+                    currentSum -= list[start];
+                    start++;
+                }
+
+                if (currentSum < sum)
+                {
+                    end++;
+                    if (end == list.Count)
+                        break;
+                    currentSum += list[end];
+                }
+            }
+            start = resultStart;
+            end = resultEnd == 0 ? resultEnd : resultEnd + 1;
         }
     }
 }
